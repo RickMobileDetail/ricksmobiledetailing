bda872640e5e56b294bbaf43c7bbe75ab56aaccf
RickMobileDetail <ricardojc346@gmail.com>
2025-04-06 21:54:10 -0500

    Remove old file

--- pages/gallery.js
+++
@@ -1,8 +0,0 @@
-export default function Gallery() {
-  return (
-    <main style={{ padding: "2rem", backgroundColor: "black", color: "white", minHeight: "100vh" }}>
-      <h1>Gallery</h1>
-      <p>Check out some of our recent work. Full photo sets coming soon!</p>
-    </main>
-  );
-}
--- pages/reviews.js
+++
@@ -1,8 +0,0 @@
-export default function Reviews() {
-  return (
-    <main style={{ padding: "2rem", backgroundColor: "black", color: "white", minHeight: "100vh" }}>
-      <h1>Reviews</h1>
-      <p>Real customer reviews coming soon. Check back for testimonials!</p>
-    </main>
-  );
-}
